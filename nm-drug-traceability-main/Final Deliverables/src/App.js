96b8de603ae92ce36cae1404764c17c58c5d426e
Harini G <128168902+Harini-882@users.noreply.github.com>
2023-10-29 23:51:08 +0530

    Add files via upload

---
+++ nm-drug-traceability-main/Final Deliverables/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'; // eslint-disable-next-line
+import React, { Component }  from 'react';
+import Home from './Page/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
